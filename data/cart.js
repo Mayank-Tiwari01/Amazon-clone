01fd956705fc977c5fb1521c5cf3a5ec29e14c52
Mayank-Tiwari01 <mayank.tiwari0106@gmail.com>
2024-05-22 04:16:42 +0530

    created html for checkout page

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,9 @@
-export const cart = [];
+export const cart = [
+  {
+    productId: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    quantity: 12
+  }
+];
 
 export function addToCart(productId, itemsSelectedValue) {
   let foundProduct = cart.find((item) => item.productId === productId);
